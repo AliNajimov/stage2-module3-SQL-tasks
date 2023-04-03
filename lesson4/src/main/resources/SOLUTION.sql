f40bdd6c7dcedae5b152e94724091e2a0164f12a
AliNajimov <najimovm11@gmail.com>
2023-04-03 11:23:53 +0500

    .done

---
+++ lesson4/src/main/resources/SOLUTION.sql
@@ -0,0 +1,4 @@
+select * from student
+selecT * from student limit 50;
+select name from student;
+select distinct amount from payment;
---
+++ lesson5/src/main/resources/SOLUTION.sql
@@ -0,0 +1,8 @@
+select * from payment where amount>=500;
+select * from student where DATEADD(year,-20,GETDATE())>=birthday;
+select * from student where groupnumber=10 and DATEADD(year,-20,GETDATE())>=birthday;
+select * from student where name='%Mike%' or groupnumber in (4,5,6);
+select * from payment where DATEADD(month,-8,GETDATE())<=payment_date
+select * from student where name like 'A%';
+select * from student where (name like 'Roxi%' and groupnumber=4) or (name like 'Tallie%' and groupnumber=9);
+
