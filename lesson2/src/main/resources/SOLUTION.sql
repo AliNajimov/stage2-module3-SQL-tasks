e5b7027f261a7e21b97e87ca43bb323864df280a
AliNajimov <najimovm11@gmail.com>
2023-04-03 10:11:14 +0500

    .done

--- lesson2/src/main/resources/SOLUTION.sql
+++ lesson2/src/main/resources/SOLUTION.sql
@@ -1,57 +1,41 @@
-insert into student (name,birthday,groupnumber) values ('John','2020-02-263','1')
-insert into student (name,birthday,groupnumber) values ('Chris','2021-02-263','1')
-insert into student (name,birthday,groupnumber) values ('Carl','2022-02-263','1')
-insert into student (name,birthday,groupnumber) values ('Oliver','2023-02-263','2')
-insert into student (name,birthday,groupnumber) values ('James','2024-02-263','2')
-insert into student (name,birthday,groupnumber) values ('Lucas','2025-02-263','2')
-insert into student (name,birthday,groupnumber) values ('Henry','2026-02-263','2')
-insert into student (name,birthday,groupnumber) values ('Jacob','2027-02-263','3')
-insert into student (name,birthday,groupnumber) values ('Logan','2028-02-263','3')
-insert into student (name,birthday,groupnumber) values ('Lucas','2029-02-263','4')
-insert into student (name,birthday,groupnumber) values ('Henry','2010-02-263','4')
-insert into student (name,birthday,groupnumber) values ('Jacob','2011-02-263','5')
-insert into student (name,birthday,groupnumber) values ('Logan','2012-02-263','5')
+insert into STUDENT(name, birthday, groupnumber) values ('John', '1974-03-09', 1);
+insert into STUDENT(name, birthday, groupnumber) values ('Chris', '1974-03-09', 1);
+insert into STUDENT(name, birthday, groupnumber) values ('Carl', '1974-03-09', 1);
+insert into STUDENT(name, birthday, groupnumber) values ('Oliver', '1974-03-09', 2);
+insert into STUDENT(name, birthday, groupnumber) values ('James', '1974-03-09', 2);
+insert into STUDENT(name, birthday, groupnumber) values ('Lucas', '1974-03-09', 2);
+insert into STUDENT(name, birthday, groupnumber) values ('Henry', '1974-03-09', 2);
+insert into STUDENT(name, birthday, groupnumber) values ('Jacob', '1974-03-09', 3);
+insert into STUDENT(name, birthday, groupnumber) values ('Logan', '1974-03-09', 3);
+insert into STUDENT(name, birthday, groupnumber) values ('Mike', '1974-03-09', 4);
+insert into STUDENT(name, birthday, groupnumber) values ('Jessey', '1974-03-09', 4);
+insert into STUDENT(name, birthday, groupnumber) values ('Pinkman', '1974-03-09', 5);
+insert into STUDENT(name, birthday, groupnumber) values ('Floyd', '1974-03-09', 5);
 
 
-insert into subject (name,grade) values ('Art','1')
-insert into subject (name,grade) values ('Music','1')
-insert into subject (name,grade) values ('Geography','2')
-insert into subject (name,grade) values ('History','2')
-insert into subject (name,grade) values ('PE','3')
-insert into subject (name,grade) values ('Math','3')
-insert into subject (name,grade) values ('Science','4')
-insert into subject (name,grade) values ('IT','4')
-insert into subject (name,grade) values ('Communication Skills','5')
-insert into subject (name,grade) values ('Physics','5')
+insert into SUBJECT(name, grade) values ('Art', 1);
+insert into SUBJECT(name, grade) values ('Music', 1);
+insert into SUBJECT(name, grade) values ('Geography', 2);
+insert into SUBJECT(name, grade) values ('History', 2);
+insert into SUBJECT(name, grade) values ('PE', 3);
+insert into SUBJECT(name, grade) values ('Math', 3);
+insert into SUBJECT(name, grade) values ('Science', 4);
+insert into SUBJECT(name, grade) values ('IT', 4);
+insert into SUBJECT(name, grade) values ('AI', 5);
+insert into SUBJECT(name, grade) values ('BDA', 5);
 
-insert into paymenttype (name) values ('DAILY')
-insert into paymenttype (name) values ('WEEKLY')
-insert into paymenttype (name) values ('MONTHLY')
+insert into PAYMENTTYPE(name) values ('DAILY');
+insert into PAYMENTTYPE(name) values ('WEEKLY');
+insert into PAYMENTTYPE(name) values ('MONTHLY');
 
-insert into payment (type_id,amount,payment_date,student_id) values ((select id from paymenttype where name='WEEKLY'),300,'2012-02-263',(select id from student where name='John'))
-insert into payment (type_id,amount,payment_date,student_id) values ((select id from paymenttype where name='MONTHLY'),300,'2012-02-263',(select id from student where name='Oliver'))
-insert into payment (type_id,amount,payment_date,student_id) values ((select id from paymenttype where name='WEEKLY'),300,'2012-02-263',(select id from student where name='Henry'))
-insert into payment (type_id,amount,payment_date,student_id) values ((select id from paymenttype where name='DAILY'),300,'2012-02-263',(select id from student where name='James'))
-insert into payment (type_id,amount,payment_date,student_id) values ((select id from paymenttype where name='WEEKLY'),300,'2012-02-263',(select id from student where name='Carl'))
-
-insert into mark (student_id, subject_id, mark) values ((select id from student where name='Chris'),(select id from subject where name='Art'),8)
-insert into mark (student_id, subject_id, mark) values ((select id from student where name='Oliver'),(select id from subject where name='History'),5)
-insert into mark (student_id, subject_id, mark) values ((select id from student where name='James'),(select id from subject where name='Geography'),9)
-insert into mark (student_id, subject_id, mark) values ((select id from student where name='Jacob'),(select id from subject where name='Math'),4)
-insert into mark (student_id, subject_id, mark) values ((select id from student where name='Logan'),(select id from subject where name='PE'),9)
-insert into mark (student_id, subject_id, mark) values ((select id from student where name='Lucas'),(select id from subject where name='Music'),7)
+insert into PAYMENT(type_id, student_id, amount, payment_date) values ((select id from PAYMENTTYPE where name = 'WEEKLY'), (select id from STUDENT where name = 'John'), 300, '2005-07-02 14:56:13');
+insert into PAYMENT(type_id, student_id, amount, payment_date) values ((select id from PAYMENTTYPE where name = 'MONTHLY'), (select id from STUDENT where name = 'Oliver'), 400, '2005-07-02 14:56:13');
+insert into PAYMENT(type_id, student_id, amount, payment_date) values ((select id from PAYMENTTYPE where name = 'WEEKLY'), (select id from STUDENT where name = 'Henry'), 500, '2005-07-02 14:56:13');
+insert into PAYMENT(type_id, student_id, amount, payment_date) values ((select id from PAYMENTTYPE where name = 'DAILY'), (select id from STUDENT where name = 'James'), 600, '2005-07-02 14:56:13');
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+insert into MARK(student_id, subject_id, mark) values ((select id from STUDENT where name = 'Chris'), (select id from SUBJECT where name = 'Art'), 8);
+insert into MARK(student_id, subject_id, mark) values ((select id from STUDENT where name = 'Oliver'),(select id from SUBJECT where name = 'History'),5);
+insert into MARK(student_id, subject_id, mark) values ((select id from STUDENT where name = 'James'), (select id from SUBJECT where name = 'Geography'), 9);
+insert into MARK(student_id, subject_id, mark) values ((select id from STUDENT where name = 'Jacob'), (select id from SUBJECT where name = 'Math'), 4);
+insert into MARK(student_id, subject_id, mark) values ((select id from STUDENT where name = 'Logan'), (select id from SUBJECT where name = 'PE'), 9);
--- lesson3/src/main/resources/SOLUTION.sql
+++ lesson3/src/main/resources/SOLUTION.sql
@@ -1,21 +1,5 @@
---alter table student alter column birthday DATE not null;
---alter table mark add constraint ch_mark check (mark.mark>=10 and mark.mark<=20);
---alter table mark alter column student_id bigint not null;
---alter table mark alter column subject_id bigint not null;
---alter table subject add constraint ch_grade check (subject.grade>=1 and subject.grade<=5);
---alter table paymenttype add constraint un_name unique (paymenttype.name);
---alter table payment alter column type_id bigint not null;
---alter table payment alter column amount decimal not null;
---alter table payment alter column payment_date DateTime not null;
---
---
---
-alter table STUDENT alter column BIRTHDAY set not null;
-alter table MARK add constraint mark_check check (MARK.MARK >= 1 and MARK.MARK <= 10);
-alter table MARK alter column SUBJECT_ID set not null;
-alter table MARK alter column STUDENT_ID set not null;
-alter table SUBJECT add constraint grade_check check (SUBJECT.GRADE >= 1 and  SUBJECT.GRADE <= 5);
-alter table PAYMENTTYPE add constraint name_unique unique(NAME);
-alter table PAYMENT alter column TYPE_ID set not null;
-alter table PAYMENT alter column AMOUNT set not null;
-alter table PAYMENT alter column PAYMENT_DATE set not null;
+alter table STUDENT modify column birthday date not null;
+alter table MARK modify column student_id bigint not null, modify column subject_id bigint not null, modify column mark int add constraint ch_mark check (mark >= 1 and mark <= 10);
+alter table SUBJECT add constraint CH_GRADE check ( grade >= 1 and grade <= 5 );
+alter table PAYMENTTYPE modify column name varchar(100) unique;
+alter table PAYMENT modify column type_id bigint not null, modify column amount decimal not null, modify column payment_date datetime not null;
